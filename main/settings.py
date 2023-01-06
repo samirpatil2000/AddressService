85848f46878568591a987e5f654e02a47ba39a80
Samir Patil <samirspatil742099@gmail.com>
2023-01-06 16:54:00 +0530

    [NEW] Cors added

--- main/settings.py
+++ main/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     # Third Party
     'crispy_forms',
     'rest_framework',
+    'corsheaders',
 
     # local app
     'address'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'main.urls'
 
